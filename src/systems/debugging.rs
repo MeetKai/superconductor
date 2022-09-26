ac87598c4ad80d6c399af8f33b622516da69a953
Ashley Ruglys <ashley.ruglys@gmail.com>
2022-09-26 13:05:04 +0200

    Move debugging systems to a module

---
+++ src/systems/debugging.rs
@@ -0,0 +1,61 @@
+use crate::components::{AnimatedModel, AnimationJoints, Instance, InstanceOf, Model};
+use crate::resources::LineBuffer;
+use bevy_ecs::prelude::{Query, ResMut};
+
+pub(crate) fn push_joints_to_lines_buffer(
+    instance_query: Query<(&InstanceOf, &AnimationJoints, &Instance)>,
+    model_query: Query<&AnimatedModel>,
+    mut line_buffer: ResMut<LineBuffer>,
+) {
+    instance_query.for_each(|(instance_of, animation_joints, instance)| {
+        match model_query.get(instance_of.0) {
+            Ok(animated_model) => {
+                for (id, (start, end)) in animation_joints
+                    .0
+                    .iter_lines(&animated_model.0.animation_data.depth_first_nodes)
+                    .enumerate()
+                {
+                    line_buffer.staging.extend_from_slice(&[
+                        renderer_core::LineVertex {
+                            position: instance.0 * start,
+                            colour_id: id as u32,
+                        },
+                        renderer_core::LineVertex {
+                            position: instance.0 * end,
+                            colour_id: id as u32,
+                        },
+                    ]);
+                }
+            }
+            Err(error) => {
+                log::warn!(
+                    "Got an error when pushing joints to the lines buffer for debugging: {}",
+                    error
+                );
+            }
+        }
+    })
+}
+
+pub(crate) fn push_bounding_boxes_to_lines_buffer(
+    instance_query: Query<(&InstanceOf, &Instance)>,
+    model_query: Query<&Model>,
+    mut line_buffer: ResMut<LineBuffer>,
+) {
+    instance_query.for_each(|(instance_of, instance)| {
+        if let Ok(model) = model_query.get(instance_of.0) {
+            for (primitive_id, primitive) in model.0.primitives.iter().enumerate() {
+                let vertices =
+                    primitive
+                        .bounding_box
+                        .line_points()
+                        .map(|point| renderer_core::LineVertex {
+                            position: instance.0 * primitive.transform * point,
+                            colour_id: primitive_id as u32,
+                        });
+
+                line_buffer.staging.extend_from_slice(&vertices);
+            }
+        }
+    })
+}
