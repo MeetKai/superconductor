cee72e1ddada1135dfe3bfbad25f0e8aa9279f00
Ashley Ruglys <ashley.ruglys@gmail.com>
2023-07-24 13:00:57 +1200

    Update deps, esp. wgpu

--- check_licenses.sh
+++ check_licenses.sh
@@ -1,6 +1,6 @@
 echo "Checking /" &&
-cargo deny --workspace --all-features check &&
+cargo deny --workspace --all-features check -A duplicate &&
 echo "Checking /shaders" &&
-cargo deny --workspace --all-features --manifest-path shaders/Cargo.toml check &&
+cargo deny --workspace --all-features --manifest-path shaders/Cargo.toml check -A duplicate &&
 echo "Checking /rust-gpu-cli-builder" &&
-cargo deny --workspace --all-features --manifest-path rust-gpu-cli-builder/Cargo.toml check
+cargo deny --workspace --all-features --manifest-path rust-gpu-cli-builder/Cargo.toml check -A duplicate
